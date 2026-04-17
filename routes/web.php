e7f575bc4434a9ce73a8a08ac700c4dff8d78145
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-17 19:26:20 +0300

    create new routes for find categories and products by slug

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php declare(strict_types=1);
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', fn () => view('index'))->name('index');
+
+Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('product.show');
