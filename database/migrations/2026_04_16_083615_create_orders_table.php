f9da33ca22fc63ba2dcf97f5c25b76e4bf13b49e
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-19 23:20:42 +0300

    adapted for product_variant_id

--- database/migrations/2026_04_16_083615_create_orders_table.php
+++ database/migrations/2026_04_16_083615_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->restrictOnDelete();
+            $table->foreignId('user_id')->constrained()->nullOnDelete();
             $table->enum('status', ['pending', 'paid', 'completed', 'cancelled'])->default('pending');
             $table->decimal('total', 8, 2);
             $table->string('name');
--- database/migrations/2026_04_16_083620_create_order_items_table.php
+++ database/migrations/2026_04_16_083620_create_order_items_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('product_id')->constrained()->restrictOnDelete();
+            $table->foreignId('product_variant_id')->constrained()->restrictOnDelete();
             $table->integer('quantity')->default(1);
             $table->decimal('price', 8, 2);
             $table->timestamps();
