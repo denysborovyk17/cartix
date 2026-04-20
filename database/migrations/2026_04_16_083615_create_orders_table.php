eaf4ff5a9420d5e85b2870c63f08a01c88468a4e
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-20 14:51:20 +0300

    user_id field can be a null

--- database/migrations/2026_04_16_083615_create_orders_table.php
+++ database/migrations/2026_04_16_083615_create_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
             $table->enum('status', ['pending', 'paid', 'completed', 'cancelled'])->default('pending');
             $table->decimal('total', 8, 2);
             $table->string('name');
