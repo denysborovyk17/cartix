ffd3338c8e4ea2dffa8d4824ff097b9e611efee5
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-25 19:16:13 +0300

    add web middleware for cart resource route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,7 @@ Route::get('/', fn () => view('index'))->name('index');
 Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('product.show');
+
+Route::middleware('web')->group(function () {
+    Route::resource('/cart', CartController::class);
+});
