cb0bb91fc8f2e3fb681813736828b31d65394ba8
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-25 20:58:14 +0300

    set slug properties for category and product instead of model objects also add checkout that product field category_id same like id of category

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    public function show(Product $product): View
+    public function show(string $categorySlug, string $productSlug): View
     {
+        $category = Category::query()->where('slug', $categorySlug)->firstOrFail();
+        $product = Product::query()->where('slug', $productSlug)
+            ->where('category_id', $category->id)
+            ->firstOrFail();
+
         $relatedProducts = Product::query()
             ->with('category')
             ->where('category_id', $product->category_id)
@@ -18,6 +23,6 @@ class ProductController extends Controller
             ->take(4)
             ->get();
 
-        return view('product', compact('product', 'relatedProducts'));
+        return view('product', compact('category', 'product', 'relatedProducts'));
     }
 }
