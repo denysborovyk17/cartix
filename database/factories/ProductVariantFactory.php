54263ef02a991c485db87b5cbda8d213ebb99c8c
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-04 19:43:17 +0300

    rename class from VariantFactory to ProductVariantFactory

--- database/factories/ProductVariantFactory.php
+++ database/factories/ProductVariantFactory.php
@@ -3,13 +3,13 @@
 namespace Database\Factories;
 
 use App\Models\Product;
-use App\Models\Variant;
+use App\Models\ProductVariant;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends Factory<Variant>
+ * @extends Factory<ProductVariant>
  */
-class VariantFactory extends Factory
+class ProductVariantFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -18,18 +18,9 @@ class VariantFactory extends Factory
      */
     public function definition(): array
     {
-        $options = [
-            'color' => fake()->randomElement(['black', 'red', 'green']),
-            'size' => fake()->randomElement(['S', 'M', 'L'])
-        ];
-        ksort($options);
-
         return [
             'product_id' => Product::factory(),
-            'options' => $options,
-            'options_hash' => hash('sha256', json_encode($options)),
             'price' => fake()->randomFloat(2, 10, 1000),
-            'discount_price' => null,
             'stock' => fake()->numberBetween(1, 100)
         ];
     }
