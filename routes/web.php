a13062195f4c7d6a91241d295978f400ce0af08d
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-08 20:50:10 +0300

    remove category slug from url for finding product by slug

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php declare(strict_types=1);
 
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\{CartController, CategoryController, ProductController};
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('index'))->name('index');
+Route::get('/', fn() => view('index'))->name('index');
 
 Route::get('/categories/{category:slug}', [CategoryController::class, 'show'])->name('categories.show');
-
-Route::get('/categories/{category:slug}/products/{product:slug}', [ProductController::class, 'show'])->name('product.show');
+Route::get('/products/{product:slug}', [ProductController::class, 'show'])->name('products.show');
 
 Route::middleware('web')->group(function () {
     Route::resource('/cart', CartController::class);
