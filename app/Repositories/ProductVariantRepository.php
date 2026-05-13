98eb1ebdf34bfb3ae0b35fa60383136cc07c3881
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-13 21:23:18 +0300

    rename class from CartRepository to ProductVariantRepository

--- app/Repositories/ProductVariantRepository.php
+++ app/Repositories/ProductVariantRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 
 use App\Models\ProductVariant;
 
-class CartRepository
+class ProductVariantRepository
 {
     public function getProductVariant(int $productVariantId): ProductVariant
     {
