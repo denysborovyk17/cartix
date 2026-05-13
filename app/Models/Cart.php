d0d03c21f293eb67143b8ac3fc867bc093a1cdde
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-13 21:06:09 +0300

    add PHPDoc for Collection of CartItem like items relationship property and create findItem method that finding cart items by product variant ID

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/** @property Collection<int, CartItem> $items */
 class Cart extends Model
 {
     protected $fillable = [
@@ -21,5 +23,12 @@ class Cart extends Model
     public function items(): HasMany
     {
         return $this->hasMany(CartItem::class);
+    }
+
+    public function findItem(int $productVariantId): CartItem|null
+    {
+        return $this->items()
+            ->where('product_variant_id', $productVariantId)
+            ->first();
     }
 }
