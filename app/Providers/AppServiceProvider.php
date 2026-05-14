3a793099f27deb1786d8eb3bfb4106cba40d394d
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-14 21:34:32 +0300

    move load variables to blade templates in ViewServiceProvider and set prevent lazy loading to avoid N + 1 problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace App\Providers;
 
-use App\Models\Category;
-use App\Services\CurrentCartService;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,18 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer(['layouts.app', 'index'], function ($view) {
-            $view->with([
-                'categories' => Category::query()
-                    ->select('id', 'name', 'slug')
-                    ->get()
-            ]);
-        });
-        View::composer('layouts.app', function ($view) {
-            $cart = app(CurrentCartService::class)->getCurrentCart();
-
-            $view->with(['cart' => $cart->load('items.productVariant.product')
-            ]);
-        });
+        Model::preventLazyLoading();
     }
 }
