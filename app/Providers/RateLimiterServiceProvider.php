fadddd99ccb7e174601f402043d15016001dcd86
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-14 16:53:28 +0300

    create RateLimiterServiceProvider for rate limit of login attempts

---
+++ app/Providers/RateLimiterServiceProvider.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\ServiceProvider;
+
+class RateLimiterServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        RateLimiter::for('login', function (Request $request) {
+            return Limit::perMinute(config('custom.rate_limiter.login'))->by($request->ip());
+        });
+    }
+}
