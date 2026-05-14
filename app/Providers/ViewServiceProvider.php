8f850f5b1187be95ed924a03ef6cede6977f1de7
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-14 21:33:04 +0300

    create ViewServiceProvider for load some variables to blade templates with using CurrentCart and Cart Services

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+namespace App\Providers;
+
+use App\Models\Category;
+use App\Services\CartService;
+use App\Services\CurrentCartService;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(CurrentCartService $currentCartService, CartService $cartService): void
+    {
+        View::composer(['layouts.app', 'index'], function ($view) {
+            $view->with([
+                'categories' => Category::query()
+                    ->whereNotNull('parent_id')
+                    ->select('id', 'name', 'slug')
+                    ->get()
+            ]);
+        });
+
+        View::composer('layouts.app', function ($view) use ($currentCartService, $cartService) {
+            $cart = $currentCartService->getCurrentCart();
+
+            $view->with([
+                'cart' => $cart->load('items.productVariant.product')
+            ]);
+
+            $view->with([
+                'cartCounter' => $cartService->getItemsCount()
+            ]);
+        });
+
+        View::composer('cart', function ($view) use ($cartService) {
+            $view->with([
+                'cartTotal' => $cartService->calculateTotal()->getAmount()
+            ]);
+        });
+    }
+}
