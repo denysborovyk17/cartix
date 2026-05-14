73fe6aaba4b071ae76044bf066744ceba5a37895
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-14 16:54:59 +0300

    add password field for user seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Denys',
             'email' => 'denys@mail.com',
+            'password' => '12345678'
         ]);
 
         $this->call([
