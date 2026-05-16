a6b9a5086512f18da072026e0199add59d60b51d
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-16 19:19:11 +0300

    add PHPDoc property for session ID

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,12 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\{Model};
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, HasMany};
 
-/** @property Collection<int, CartItem> $items */
+/** @property string $session_id */
 class Cart extends Model
 {
     protected $fillable = [
