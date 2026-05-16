c9885217cb93df9747c45fd46d7ab7710e3e00a5
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-16 19:20:03 +0300

    register Login event with MergeGuestCart listener

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,26 @@
 
 namespace App\Providers;
 
+use App\Listeners\MergeGuestCart;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Model::preventLazyLoading();
+        Model::preventLazyLoading(! $this->app->isProduction());
+
+        Event::listen(
+            Login::class,
+            MergeGuestCart::class
+        );
     }
 }
