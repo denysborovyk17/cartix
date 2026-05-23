570e63982daa24e79e9ef8585a42058769638c58
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-24 00:19:47 +0300

    change foreign key from product id to product variant id

--- database/migrations/2026_04_16_083630_create_wishlist_items_table.php
+++ database/migrations/2026_04_16_083630_create_wishlist_items_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('wishlist_items', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('product_variant_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
 
-            $table->unique(['user_id', 'product_id']);
+            $table->unique(['user_id', 'product_variant_id']);
         });
     }
 
