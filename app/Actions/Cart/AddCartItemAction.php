d35f44ecc12d9fddecdb83f0dc9a2fedcba423dd
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-24 18:31:58 +0300

    create separate actions with some logic which was removed from CartService

---
+++ app/Actions/Cart/AddCartItemAction.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+namespace App\Actions\Cart;
+
+use App\Services\CartService;
+use App\Services\CurrentCartService;
+
+readonly class AddCartItemAction
+{
+    public function __construct(
+        private CurrentCartService $currentCartService,
+        private CartService $cartService
+    ) {}
+
+    public function handle(int $productVariantId, int $quantity = 1): array
+    {
+        $cart = $this->currentCartService->findById();
+
+        $cartItem = $cart->findItemByProductVariantId($productVariantId);
+
+        if ($cartItem) {
+            $cartItem->quantity++;
+        } else {
+            $cartItemData = [
+                'product_variant_id' => $productVariantId,
+                'quantity' => $quantity
+            ];
+            $cartItem = $cart->items()->create($cartItemData);
+        }
+        $cartItem->save();
+        $cartItem->load('productVariant.optionValues.option');
+
+        return [
+            'cartItem' => $cartItem,
+            'cartCounter' => $this->cartService->getItemsCount()
+        ];
+    }
+}
