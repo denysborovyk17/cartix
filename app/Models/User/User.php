b91e0007cb956f1c5a081a0c2afe7d30b2d1a2f5
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-24 20:23:37 +0300

    add comments to properties for better context and fix relationship from cartItem to cart

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -4,19 +4,19 @@ namespace App\Models\User;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Enums\UserRole;
-use App\Models\Cart\CartItem;
+use App\Models\Cart\Cart;
 use App\Models\Order\Order;
 use App\Models\Product\Review;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\{HasMany, HasOne};
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 /**
- * @property string $name
- * @property string $email
- * @property string $password
- * @property UserRole $role
+ * @property string $name Ім'я користувача
+ * @property string $email Електронна адреса користувача
+ * @property string $password Пароль користувача
+ * @property UserRole $role Роль користувача
  */
 class User extends Authenticatable
 {
@@ -44,9 +44,9 @@ class User extends Authenticatable
         ];
     }
 
-    public function cartItems(): HasMany
+    public function cart(): HasOne
     {
-        return $this->hasMany(CartItem::class);
+        return $this->hasOne(Cart::class);
     }
 
     public function orders(): HasMany
