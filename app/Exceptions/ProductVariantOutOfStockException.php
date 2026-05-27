021c359ac9501717260fc2749ed8d632bccfd17c
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-27 15:59:54 +0300

    create ProductVariantOutOfStockException

---
+++ app/Exceptions/ProductVariantOutOfStockException.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ProductVariantOutOfStockException extends Exception
+{
+    public function __construct(protected int $productVariantId)
+    {
+        $message = "Product Variant with ID {$productVariantId} is out of stock.";
+
+        parent::__construct($message);
+    }
+}
