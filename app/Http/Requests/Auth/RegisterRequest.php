1178382535be6bde4364af0dd6c4c0bb41fa50c9
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-27 13:51:34 +0300

    style: format code and add password confirmation

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -24,9 +24,9 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:255',
-            'email' => 'bail|required|unique:users|string|email|max:255',
-            'password' => 'required|string|min:12',
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['bail', 'required', 'unique:users,email', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'min:12', 'confirmed']
         ];
     }
 
