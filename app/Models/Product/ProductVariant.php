a109fc1bb55b7e73f4f297e8d925994591b9c237
Denys Borovyk <borovikdenis748@gmail.com>
2026-05-31 17:07:19 +0300

    style used files

--- app/Models/Product/ProductVariant.php
+++ app/Models/Product/ProductVariant.php
@@ -4,8 +4,7 @@ namespace App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, BelongsToMany};
 
 /**
  * @property int $product_id Унікальний ідентифікатор товару
