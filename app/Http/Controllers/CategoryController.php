98054e02f4e80c604a8c3f967b4207d98c04b9e6
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-05 16:00:08 +0300

    inject ProductRepository, fill findBySearch method by data from input field, add pagination and passed to view blade template

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,19 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\CategoryRepository;
+use App\Repositories\ProductRepository;
+use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CategoryController extends Controller
 {
     public function __construct(
-        private readonly CategoryRepository $categoryRepository
+        private readonly CategoryRepository $categoryRepository,
+        private readonly ProductRepository $productRepository
     ) {
     }
 
-    public function show(string $slug): View
+    public function show(Request $request, string $slug): View
     {
         $category = $this->categoryRepository->findBySlug($slug);
 
-        return view('category', compact('category'));
+        $products = $this->productRepository->findBySearch(trim((string) $request->input('search')), $category->id);
+
+        return view('category', compact('category', 'products'));
     }
 }
