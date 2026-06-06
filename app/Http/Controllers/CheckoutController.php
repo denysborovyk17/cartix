470ed4ad024a1505a4e98b15daf1729e56b59a12
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-06 19:28:39 +0300

    remove methods for view templates and complete order to OrderController

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Actions\Checkout\{CreateOrderAction, ConfirmPaymentAction};
+use App\Actions\Checkout\CreateOrderAction;
 use App\Http\Requests\StoreCheckoutRequest;
-use App\Repositories\OrderRepository;
 use App\Services\Cart\CurrentCartService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
@@ -13,8 +12,7 @@ use Throwable;
 class CheckoutController extends Controller
 {
     public function __construct(
-        private readonly CurrentCartService $currentCartService,
-        private readonly OrderRepository $orderRepository
+        private readonly CurrentCartService $currentCartService
     ) {
     }
 
@@ -32,20 +30,6 @@ class CheckoutController extends Controller
     {
         $order = $action->handle($request->toDTO());
 
-        return redirect()->route('checkout.payment', ['orderId' => $order->id]);
-    }
-
-    public function showPayment(int $orderId): View
-    {
-        $order = $this->orderRepository->findById($orderId);
-
-        return view('checkout.checkout-payment', compact('order'));
-    }
-
-    public function confirmPayment(ConfirmPaymentAction $action, int $orderId): RedirectResponse
-    {
-        $action->handle($orderId);
-
-        return redirect()->route('index');
+        return redirect()->route('orders.payment', $order->id);
     }
 }
