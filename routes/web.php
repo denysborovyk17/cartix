5a133faae1dfe1efea2696bbdcfc2311b46bcdfd
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-06 19:30:02 +0300

    update routes naming and adapted for new naming in controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-use App\Http\Controllers\{CartController, CategoryController, ProductController, CheckoutController};
+use App\Http\Controllers\{CartController, CategoryController, ProductController, CheckoutController, OrderController};
 use App\Http\Controllers\Auth\{RegisterController, LoginController, LogoutController, ForgotPasswordController, ResetPasswordController};
 use Illuminate\Support\Facades\Route;
 
@@ -22,15 +22,19 @@ Route::middleware('guest')->group(function () {
         Route::post('/login', LoginController::class)->middleware('throttle:login')->name('login.attempt');
     });
 
-    Route::get('/forgot-password', [ForgotPasswordController::class, 'showForgotForm'])->name('password.request');
-    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLink'])->name('password.email');
-    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-    Route::post('/reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.update');
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'create'])->name('password.request');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'store'])->name('password.email');
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'edit'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'update'])->name('password.update');
 });
 
 Route::post('/logout', LogoutController::class)->middleware('auth')->name('logout');
 
 Route::get('/checkout', [CheckoutController::class, 'index'])->name('checkout');
 Route::post('/checkout', [CheckoutController::class, 'store'])->name('checkout.store');
-Route::get('/checkout-payment/{orderId}', [CheckoutController::class, 'showPayment'])->name('checkout.payment');
-Route::post('/checkout-payment/{orderId}/success', [CheckoutController::class, 'confirmPayment'])->name('checkout.payment.store');
+
+Route::prefix('orders/{orderId}')->as('orders.')->group(function () {
+    Route::get('/payment', [OrderController::class, 'show'])->name('payment');
+    Route::post('/payment/complete', [OrderController::class, 'complete'])->name('payment.complete');
+    Route::get('/success', [OrderController::class, 'success'])->name('success');
+});
