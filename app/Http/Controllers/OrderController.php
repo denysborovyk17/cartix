c3f078d972815c116f09e0563b22ca0509d8e165
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-06 19:27:25 +0300

    create OrderController with methods for view templates and complete order status

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Actions\Checkout\ConfirmPaymentAction;
+use App\Repositories\OrderRepository;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class OrderController extends Controller
+{
+    public function __construct(
+        private readonly OrderRepository $orderRepository
+    ) {
+    }
+
+    public function show(int $orderId): View
+    {
+        $order = $this->orderRepository->findById($orderId);
+
+        return view('checkout.checkout-payment', compact('order'));
+    }
+
+    public function complete(ConfirmPaymentAction $action, int $orderId): RedirectResponse
+    {
+        $action->handle($orderId);
+
+        return redirect()->route('orders.success', $orderId);
+    }
+
+    public function success(int $orderId): View
+    {
+        $order = $this->orderRepository->findById($orderId);
+
+        return view('checkout.checkout-success', compact('order'));
+    }
+}
