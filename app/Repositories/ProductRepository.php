2a4ca445c7d5fc5206f0ec99e8818caca7053b82
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-09 17:30:13 +0300

    add methods to get brands, colors, sizes and findBySearchAndFilter with changed search and filter system using by Eloquent ORM with LIKE operator

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -2,7 +2,8 @@
 
 namespace App\Repositories;
 
-use App\Models\Product\Product;
+use App\DTO\ProductSearchFilterData;
+use App\Models\Product\{Product, Brand, OptionValue};
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
@@ -29,13 +30,63 @@ class ProductRepository
             ->get();
     }
 
-    public function findBySearch(string $search, int $categoryId): LengthAwarePaginator
+    public function getBrands(): Collection
     {
-        return Product::search($search)->query(function ($query) {
-            $query->with('variants');
-        })
+        return Brand::query()->get();
+    }
+
+    public function getColors(): array
+    {
+        return OptionValue::query()
+            ->whereHas('option', fn($q) => $q->where('name', 'Color'))
+            ->pluck('value')
+            ->toArray();
+    }
+
+    public function getSizes(): array
+    {
+        return OptionValue::query()
+            ->whereHas('option', fn($q) => $q->where('name', 'Size'))
+            ->pluck('value')
+            ->toArray();
+    }
+
+    public function findBySearchAndFilter(ProductSearchFilterData $data, int $categoryId): LengthAwarePaginator
+    {
+        return Product::query()
+            ->with('variants.optionValues')
             ->where('category_id', $categoryId)
-            ->paginate(12)
+
+            ->when($data->getSearch(),
+                fn($q) => $q->where(
+                    fn($q) => $q->whereLike('name', "%{$data->getSearch()}%")
+                        ->orWhereLike('description', "%{$data->getSearch()}%")
+                )
+            )
+
+            ->when($data->getBrands(),
+                fn($q) => $q->whereIn('brand_id', $data->getBrands()
+                )
+            )
+
+            ->when($data->getMinPrice() || $data->getMaxPrice(),
+                fn($q) => $q->whereHas('variants',
+                    fn($q) => $q->whereBetween('price', [$data->getMinPrice() ?? 0, $data->getMaxPrice() ?? 99999999])
+                )
+            )
+
+            ->when($data->getColors(),
+                fn($q) => $q->whereHas('variants.optionValues',
+                    fn($q) => $q->whereIn('value', $data->getColors())
+                )
+            )
+
+            ->when($data->getSizes(),
+                fn($q) => $q->whereHas('variants.optionValues',
+                    fn($q) => $q->whereIn('value', $data->getSizes())
+                )
+            )
+            ->paginate($data->getPerPage())
             ->withQueryString();
     }
 }
