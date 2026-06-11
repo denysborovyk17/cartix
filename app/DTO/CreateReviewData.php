d90b1e8da8e399eecf2b44048197984b6230eb48
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-11 18:27:20 +0300

    create CreateReviewData for control data type for creating a review

---
+++ app/DTO/CreateReviewData.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace App\DTO;
+
+readonly class CreateReviewData
+{
+    public function __construct(
+        private int $rating,
+        private string $comment,
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            rating: (int) $data['rating'],
+            comment: $data['comment']
+        );
+    }
+
+    public function getRating(): int
+    {
+        return $this->rating;
+    }
+
+    public function getComment(): string
+    {
+        return $this->comment;
+    }
+}
