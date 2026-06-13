69cb18a52d7bbfd46dbf49abb701244ec52651d3
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-13 17:59:36 +0300

    remove html code

--- public/js/cart.js
+++ public/js/cart.js
@@ -62,41 +62,8 @@
 
                 const existingCartItem = document.querySelector(`.cart-item[data-product-variant-id="${productVariantId}"]`);
                 if (existingCartItem) {
-                    // existingCartItem.outerHTML = data.cartItem; (View)
                     const cartItemQuantity = existingCartItem.querySelector('.cart-item-quantity');
                     cartItemQuantity.textContent = `Quantity (${data.cartItem.quantity})`;
-                } else {
-                    const cartContainer = document.querySelector('#cart-container');
-                    const cartItemOptionsValue = data.cartItem.options.map(optionValue => optionValue.value).join(' + ');
-                    const html = `
-                    <div class="row mx-0 py-4 g-0 border-bottom cart-item" data-product-variant-id="${data.cartItem.product_variant_id}">
-                        <div class="col-2 position-relative">
-                            <picture class="d-block ">
-                                <img class="img-fluid" src="${data.cartItem.image}" alt="">
-                            </picture>
-                        </div>
-                        <div class="col-9 offset-1">
-                            <div>
-                                <h6 class="justify-content-between d-flex align-items-start mb-2">
-                                    ${data.cartItem.name}
-                                    <i class="ri-close-line ms-3"></i>
-                                </h6>
-                                <span class="d-block text-muted fw-bolder text-uppercase fs-9 cart-item-quantity">
-                                    Quantity (${data.cartItem.quantity})
-                                </span>
-                            </div>
-                            <small class="text-muted d-block">
-                                ${cartItemOptionsValue}
-                            </small>
-                            <p class="fw-bolder text-end text-muted m-0">
-                                ${data.cartItem.price.formatted}
-                            </p>
-                        </div>
-                    </div>`;
-
-                    if (cartContainer) {
-                        cartContainer.insertAdjacentHTML('beforeend', html); // замість html => data.cartItem (View)
-                    }
                 }
 
             } catch (error) {
