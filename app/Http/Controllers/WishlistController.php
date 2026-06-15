90effc628508399836788f1fc3c09db47bcba6ef
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-15 09:39:17 +0300

    create WishlistController for show blade template and toggling wishlist items

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Actions\User\ToggleWishlistItemAction;
+use Illuminate\Http\JsonResponse;
+use Illuminate\View\View;
+
+final readonly class WishlistController
+{
+    public function __construct(
+        //
+    ) {
+    }
+
+    public function index(): View
+    {
+        $user = auth()->user();
+
+        $wishlistItems = $user->wishlistItems()->paginate(12);
+        $wishlistItems->load('product.variants');
+
+        return view('wishlist', compact('wishlistItems'));
+    }
+
+    public function toggle(ToggleWishlistItemAction $action, int $productVariantId): JsonResponse
+    {
+        $action = $action->handle($productVariantId);
+
+        return response()->json([
+            'wishlistCounter' => $action['wishlistCounter'],
+            'status' => $action['status']
+        ]);
+    }
+}
