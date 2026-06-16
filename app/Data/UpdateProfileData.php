08d9698764aaf1329812f302c6f0febac51edd02
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-16 11:46:19 +0300

    create dto for typization profile data

---
+++ app/Data/UpdateProfileData.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace App\Data;
+
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
+use Illuminate\Http\UploadedFile;
+use Propaganistas\LaravelPhone\PhoneNumber;
+
+readonly class UpdateProfileData
+{
+    public function __construct(
+        private string $name,
+        private ?UploadedFile $avatarPath,
+        private ?PhoneNumber $phone,
+        private ?CarbonInterface $birthday,
+        private bool $removeAvatarPath
+    ) {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            name: $data['name'],
+            avatarPath: $data['avatar_path'] ?? null,
+            phone: !empty($data['phone']) ? new PhoneNumber($data['phone'], 'UA') : null,
+            birthday: !empty($data['birthday']) ? Carbon::parse($data['birthday']) : null,
+            removeAvatarPath: !empty($data['remove_avatar_path'])
+        );
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAvatarPath(): UploadedFile|null
+    {
+        return $this->avatarPath;
+    }
+
+    public function getPhone(): PhoneNumber|null
+    {
+        return $this->phone;
+    }
+
+    public function getBirthday(): CarbonInterface|null
+    {
+        return $this->birthday;
+    }
+
+    public function getRemoveAvatarPath(): bool
+    {
+        return $this->removeAvatarPath;
+    }
+}
