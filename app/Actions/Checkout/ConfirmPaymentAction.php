d174fae74dbef1157968c48ab817c704860c1575
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-16 12:46:36 +0300

    add decrementing stock of product variant after orders completing

--- app/Actions/Checkout/ConfirmPaymentAction.php
+++ app/Actions/Checkout/ConfirmPaymentAction.php
@@ -3,8 +3,12 @@
 namespace App\Actions\Checkout;
 
 use App\Enums\OrderStatus;
+use App\Exceptions\ProductVariantOutOfStockException;
+use App\Models\Order\Order;
 use App\Repositories\OrderRepository;
 use App\Services\Cart\CurrentCartService;
+use Illuminate\Support\Facades\DB;
+use Throwable;
 
 readonly class ConfirmPaymentAction
 {
@@ -14,14 +18,32 @@ readonly class ConfirmPaymentAction
     ) {
     }
 
-    public function handle(int $orderId): void
+    /**
+     * @throws Throwable
+     */
+    public function handle(int $orderId): Order
     {
-        $order = $this->orderRepository->findById($orderId);
-        $order->update([
-            'status' => OrderStatus::PAID
-        ]);
+        return DB::transaction(function () use ($orderId): Order  {
+            $order = $this->orderRepository->findById($orderId);
 
-        $cart = $this->currentCartService->findById();
-        $cart->items()->delete();
+            foreach ($order->items as $orderItem) {
+                $productVariant = $orderItem->productVariant()->lockForUpdate()->first();
+
+                if ($productVariant->stock < $orderItem->quantity) {
+                    throw new ProductVariantOutOfStockException($productVariant->id);
+                }
+
+                $productVariant->decrement('stock', $orderItem->quantity);
+            }
+
+            $order->update([
+                'status' => OrderStatus::PAID
+            ]);
+
+            $cart = $this->currentCartService->findById();
+            $cart->items()->delete();
+
+            return $order;
+        });
     }
 }
