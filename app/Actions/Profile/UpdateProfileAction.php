3a61e849c776cec5fed58e1ceb2af04ad1ba0ae4
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-22 22:21:08 +0300

    adapted for changed FileService

--- app/Actions/Profile/UpdateProfileAction.php
+++ app/Actions/Profile/UpdateProfileAction.php
@@ -4,20 +4,22 @@ namespace App\Actions\Profile;
 
 use App\Data\UpdateProfileData;
 use App\Models\User\User;
-use App\Services\AvatarService;
+use App\Repositories\UserRepository;
+use App\Services\FileService;
 
 readonly class UpdateProfileAction
 {
     public function __construct(
-        private AvatarService $avatarService
+        private UserRepository $userRepository,
+        private FileService $fileService
     ) {
     }
 
-    public function handle(UpdateProfileData $data): User
+    public function handle(UpdateProfileData $data, int $userId): User
     {
-        $user = auth()->user();
+        $user = $this->userRepository->findById($userId);
 
-        $avatarPath = $this->avatarService->updateForUser($data, $user->avatar_path);
+        $avatarPath = $this->fileService->update($data->getRemoveAvatarPath(), $data->getAvatarPath(), $user->avatar_path);
 
         $user->update([
             'name' => $data->getName(),
