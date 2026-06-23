3469bb961f1ce4b3fa78e16dc9c64e30de7d2c67
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-23 18:51:30 +0300

    style part with FileService

--- app/Actions/Admin/Brand/UpdateBrandAction.php
+++ app/Actions/Admin/Brand/UpdateBrandAction.php
@@ -5,24 +5,25 @@ namespace App\Actions\Admin\Brand;
 use App\Data\Admin\BrandData;
 use App\Models\Product\Brand;
 use App\Repositories\BrandRepository;
-use App\Services\FileService;
+use App\Services\{FileService, SlugService};
 
 readonly class UpdateBrandAction
 {
     public function __construct(
         private BrandRepository $brandRepository,
-        private FileService $fileService
+        private FileService $fileService,
+        private SlugService $slugService
     ) {
     }
 
     public function handle(BrandData $data, int $brandId): Brand
     {
         $brand = $this->brandRepository->findById($brandId);
-
         $imagePath = $this->fileService->update($data->getRemoveImage(), $data->getImage(), $brand->image);
 
         $brand->update([
             'name' => $data->getName(),
+            'slug' => $this->slugService->generateUnique($data->getName(), new Brand(), $brand->id) ?? $brand->slug,
             'image' => $imagePath
         ]);
 
