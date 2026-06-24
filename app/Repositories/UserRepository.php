5f962ab91624689c664948af1e7c370f47559351
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-24 11:24:21 +0300

    add method for get all users

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -3,11 +3,17 @@
 namespace App\Repositories;
 
 use App\Models\User\User;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 readonly class UserRepository
 {
     public function findById(int $userId): User
     {
         return User::query()->findOrFail($userId);
+    }
+
+    public function getUsers(): LengthAwarePaginator
+    {
+        return User::query()->paginate(config('custom.pagination.per_page'));
     }
 }
