30bc6d179d711d6adbd7aa8605b3a5b66e245ff9
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-24 16:12:27 +0300

    add link for view with option values table

--- resources/views/sbadmin2/components/sidebar.blade.php
+++ resources/views/sbadmin2/components/sidebar.blade.php
@@ -47,6 +47,11 @@
             <i class="fas fa-fw fa-table"></i>
             <span>Options Table</span></a>
     </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin.option-values.index') }}">
+            <i class="fas fa-fw fa-table"></i>
+            <span>Option Values Table</span></a>
+    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
