3697a65a76b9afec6ae98d08a2ccb5c1963bc1f1
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-24 12:35:15 +0300

    create OptionRepository for getting options and finding by id

---
+++ app/Repositories/OptionRepository.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Product\Option;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+readonly class OptionRepository
+{
+    public function __construct(
+        //
+    ) {
+    }
+
+    public function getAll(): LengthAwarePaginator
+    {
+        return Option::query()->paginate(config('custom.pagination.per_page'));
+    }
+
+    public function findById(int $optionId): Option
+    {
+        return Option::query()->findOrFail($optionId);
+    }
+}
