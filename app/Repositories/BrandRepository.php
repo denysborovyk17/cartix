3dbe89945a241d0f6db8b495e244a7bee697899d
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-26 18:40:19 +0300

    rename method for pagination and add method for finding brand by name

--- app/Repositories/BrandRepository.php
+++ app/Repositories/BrandRepository.php
@@ -3,11 +3,17 @@
 namespace App\Repositories;
 
 use App\Models\Product\Brand;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 readonly class BrandRepository
 {
-    public function getBrands(): Collection
+    public function getAll(): LengthAwarePaginator
+    {
+        return Brand::query()->paginate(config('custom.pagination.per_page'));
+    }
+
+    public function getAllCollection(): Collection
     {
         return Brand::query()->get();
     }
@@ -15,5 +21,10 @@ readonly class BrandRepository
     public function findById(int $brandId): Brand
     {
         return Brand::query()->findOrFail($brandId);
+    }
+
+    public function findByName(string $name): Brand
+    {
+        return Brand::query()->where('name', $name)->firstOrFail();
     }
 }
