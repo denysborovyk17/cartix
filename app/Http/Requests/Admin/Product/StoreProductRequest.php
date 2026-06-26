c3e470ccad0b3380cc87e0a99bda426f4e40adc8
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-26 18:57:11 +0300

    create requests for creating and updating product

---
+++ app/Http/Requests/Admin/Product/StoreProductRequest.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests\Admin\Product;
+
+use App\Data\Admin\ProductData;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'category' => ['required', 'string', 'exists:categories,name'],
+            'brand' => ['required', 'string', 'exists:brands,name'],
+            'name' => ['nullable', 'required_without:existing_product_name', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'min:20'],
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,webp', 'max:2048'],
+            'is_active' => ['nullable', 'boolean'],
+            'existing_product_name' => ['nullable', 'required_without:name', 'string', 'exists:products,name'],
+            'price' => ['required', 'integer'],
+            'discount_price' => ['nullable', 'int'],
+            'currency' => ['required', 'string', 'in:UAH,USD,EUR'],
+            'stock' => ['required', 'int'],
+            'options' => ['nullable', 'array', 'exists:options,name'],
+            'option_values' => ['nullable', 'array', 'exists:option_values,value']
+        ];
+    }
+
+    public function getData(): ProductData
+    {
+        return ProductData::fromArray($this->validated());
+    }
+}
