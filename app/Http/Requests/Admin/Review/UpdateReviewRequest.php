110150b89fc2cdc1364f6b563f3fd6c6d1b3aa4a
Denys Borovyk <borovikdenis748@gmail.com>
2026-06-30 12:10:52 +0300

    create UpdateReviewRequest for validation review data during the updating

---
+++ app/Http/Requests/Admin/Review/UpdateReviewRequest.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Requests\Admin\Review;
+
+use App\Data\Admin\UpdateReviewData;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'comment' => ['nullable', 'string']
+        ];
+    }
+
+    public function getData(): UpdateReviewData
+    {
+        return UpdateReviewData::fromArray($this->validated());
+    }
+}
